39d5bd18ae830f83fc062989bc00f38e2b9dd394
Joao Silva <josejoaosilva.n@gmail.com>
2021-03-16 11:17:49 -0300

    create landscapeEntity and tests

---
+++ CordelGameTests/LandscapeEntityTests.swift
@@ -0,0 +1,18 @@
+//
+//  LandscapeEntityTests.swift
+//  CordelGameTests
+//
+//  Created by José João Silva Nunes Alves on 16/03/21.
+//
+
+import XCTest
+@testable import CordelGame
+
+class LandscapeEntityTests: XCTestCase {
+
+    func test_create_notNil() {
+        let visualComponent = LandscapeEntity()
+
+        XCTAssertNotNil(visualComponent)
+    }
+}
