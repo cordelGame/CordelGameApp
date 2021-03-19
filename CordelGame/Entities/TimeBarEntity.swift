dd3a7f717a977750c1950c898a9e30d0514bf71d
Helaine <helaine.alves.pontes07@aluno.ifce.edu.br>
2021-03-19 14:44:02 -0300

    TimeBarEntity created

---
+++ CordelGame/Entities/TimeBarEntity.swift
@@ -0,0 +1,25 @@
+//
+//  TimeBarEntity.swift
+//  CordelGame
+//
+//  Created by Helaine Pontes on 17/03/21.
+//
+
+import Foundation
+import GameplayKit
+
+class TimeBarEntity: GKEntity {
+
+    init(_ assetName: String) {
+        super.init()
+        let visualComponent = VisualComponent(assetName: assetName)
+        self.addComponent(visualComponent)
+        
+        let timeComponent = TimeComponent(node: visualComponent.node)
+        self.addComponent(timeComponent)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
