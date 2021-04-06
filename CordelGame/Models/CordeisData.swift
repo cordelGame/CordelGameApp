b7fd97cde2687a07ca0b1be5599f6e5731331e5f
Joao Silva <josejoaosilva.n@gmail.com>
2021-04-06 09:42:16 -0300

    add cordeisImages

--- CordelGame/Models/CordeisData.swift
+++ CordelGame/Models/CordeisData.swift
@@ -8,16 +8,68 @@
 import Foundation
 
 struct CordeisData {
-    static let easy: [Cordel] = [Cordel(name: "Cordel", winCondition:
-                                            [["button1", "button2", "button3", "button4"],
-                                             ["button4", "button3", "button2", "button1"]]),
-                                 Cordel(name: "Calango(1)", winCondition:
-                                            [["button1", "button2", "button3"],
-                                            ["button3", "button2", "button1"]])]
-    static let medium: [Cordel] = [Cordel(name: "Cordel", winCondition:
-                                            [["button1", "button2", "button3", "button4"],
-                                             ["button4", "button3", "button2", "button1"]])]
-    static let hard: [Cordel] = [Cordel(name: "Cordel", winCondition:
-                                            [["button1", "button2", "button3", "button4"],
-                                             ["button4", "button3", "button2", "button1"]])]
+    static let easy: [Cordel] = [ Cordel(name: "Draw01",
+                                         winCondition: [["button1", "button4", "button3"],
+                                                        ["button3", "button4", "button1"]]),
+                                  Cordel(name: "Draw02",
+                                         winCondition: [["button2", "button3", "button4"],
+                                                        ["button4", "button3", "button2"]]),
+                                  Cordel(name: "Draw03",
+                                         winCondition: [["button1", "button3", "button2"],
+                                                        ["button2", "button3", "button1"]]),
+                                  Cordel(name: "Draw04",
+                                         winCondition: [["button1", "button2", "button4"],
+                                                        ["button4", "button2", "button1"]]),
+                                  Cordel(name: "Draw05",
+                                         winCondition: [["button4", "button1", "button3"],
+                                                        ["button3", "button1", "button4"]])
+                                ]
+
+    static let medium: [Cordel] = [ Cordel(name: "Draw06",
+                                           winCondition: [["button1", "button2", "button4", "button1"],
+                                                          ["button1", "button4", "button2", "button1"],
+                                                          ["button2", "button4", "button1", "button2"],
+                                                          ["button2", "button1", "button4", "button2"],
+                                                          ["button4", "button1", "button2", "button4"],
+                                                          ["button4", "button2", "button1", "button4"]]),
+                                    Cordel(name: "Draw07",
+                                           winCondition: [["button1", "button2", "button3", "button1"],
+                                                          ["button1", "button3", "button2", "button1"],
+                                                          ["button2", "button3", "button1", "button2"],
+                                                          ["button2", "button1", "button3", "button2"],
+                                                          ["button3", "button1", "button2", "button3"],
+                                                          ["button3", "button2", "button1", "button3"]]),
+                                    Cordel(name: "Draw08",
+                                           winCondition: [["button1", "button2", "button3", "button4"],
+                                                          ["button4", "button3", "button2", "button1"]]),
+                                    Cordel(name: "Draw09",
+                                           winCondition: [["button1", "button4", "button3", "button2"],
+                                                          ["button2", "button3", "button4", "button1"]])
+                                  ]
+
+    static let hard: [Cordel] = [ Cordel(name: "Draw10",
+                                         winCondition: [["button2", "button4", "button1", "button3"],
+                                                        ["button3", "button1", "button4", "button2"]]),
+                                  Cordel(name: "Draw11",
+                                         winCondition: [["button4", "button2", "button1", "button3"],
+                                                        ["button3", "button1", "button2", "button4"]]),
+                                  Cordel(name: "Draw12",
+                                         winCondition: [["button1", "button2", "button4", "button3", "button1"],
+                                                        ["button1", "button3", "button4", "button2", "button1"],
+                                                        ["button2", "button4", "button3", "button1", "button2"],
+                                                        ["button2", "button1", "button3", "button4", "button2"],
+                                                        ["button3", "button4", "button2", "button1", "button3"],
+                                                        ["button3", "button1", "button2", "button4", "button3"],
+                                                        ["button4", "button2", "button1", "button3", "button4"],
+                                                        ["button4", "button3", "button1", "button2", "button4"]]),
+                                  Cordel(name: "Draw13",
+                                         winCondition: [["button1", "button3", "button2", "button4", "button1"],
+                                                        ["button1", "button4", "button2", "button3", "button1"],
+                                                        ["button2", "button3", "button1", "button4", "button2"],
+                                                        ["button2", "button4", "button1", "button3", "button2"],
+                                                        ["button3", "button1", "button4", "button2", "button3"],
+                                                        ["button3", "button2", "button4", "button1", "button3"],
+                                                        ["button4", "button2", "button3", "button1", "button4"],
+                                                        ["button4", "button1", "button3", "button2", "button4"]])
+                                ]
 }
