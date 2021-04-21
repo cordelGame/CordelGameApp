2c2f8808140a96711eba65f168574a4f7529c9ac
CrisCoutinho <criscoutinho44@gmail.com>
2021-04-21 09:27:10 -0300

    Update CharacterEntity and Tests
    CharacterEntity:
    - Implemented Required init?
    
    Tests:
    - Refactor code to include setUp() and tearDown()
    - create test_deathCharacter_callDeath
    - create test_initWithCoder_returnNil

--- CordelGame/Entities/CharacterEntity.swift
+++ CordelGame/Entities/CharacterEntity.swift
@@ -31,7 +31,7 @@ class CharacterEntity: GKEntity {
   }
 
   required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
+    return nil
   }
 
 }
--- CordelGameTests/CharacterEntityTests.swift
+++ CordelGameTests/CharacterEntityTests.swift
@@ -9,11 +9,28 @@ import XCTest
 @testable import CordelGame
 
 class CharacterEntityTests: XCTestCase {
+  var sut: CharacterEntity!
+
+  override func setUp() {
+    super.setUp()
+
+    sut = CharacterEntity(assetName: "Cobra", health: 2)
+  }
 
   func test_init_notNil() {
-    let sut: CharacterEntity = CharacterEntity(assetName: "Cobra", health: 2)
-
     XCTAssertNotNil(sut)
   }
 
+  func test_deathCharacter_callDeath() {
+    XCTAssertNoThrow(sut.deathCharacter())
+  }
+
+  func test_initWithCoder_returnNil() {
+    sut = CharacterEntity(coder: NSCoder())
+
+      XCTAssertNil(sut)
+  }
+  override func tearDown() {
+    sut = nil
+  }
 }
